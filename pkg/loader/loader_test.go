8aefaff593514f231ff8c139e4e68df87d2b6cac
Raywall Malheiros <raywall.malheiros@gmail.com>
2025-04-18 19:32:18 -0300

    testes unitários

--- pkg/loader/loader_test.go
+++ pkg/loader/loader_test.go
@@ -1,132 +1,54 @@
 package loader
 
-// import (
-// 	"context"
-// 	"fmt"
-// 	"io"
-// 	"os"
-// 	"strings"
-// 	"testing"
+import (
+	"context"
+	"fmt"
+	"os"
+	"testing"
 
-// 	"github.com/aws/aws-sdk-go-v2/config"
-// 	"github.com/aws/aws-sdk-go-v2/service/s3"
-// 	"github.com/aws/aws-sdk-go-v2/service/ssm"
-// 	"github.com/aws/aws-sdk-go-v2/service/ssm/types"
-// 	"github.com/aws/aws-sdk-go/aws"
-// 	"github.com/stretchr/testify/assert"
-// 	"github.com/stretchr/testify/mock"
-// )
+	awsConfig "github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/raywall/aws-lowcode-lambda-go/pkg/loader/config"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
 
-// var (
-// 	local     string = "template.yaml"
-// 	bucket    string = "my_s3_bucket"
-// 	parameter string = "my_parameter_store"
-// )
+var (
+	local    string = "/Users/macmini/Documents/workspace/go/aws-lowcode-lambda-go/examples"
+	filename string = "template.yaml"
+	content  []byte
+)
 
-// // MockS3Client é um mock para o cliente S3
-// type MockS3Client struct {
-// 	mock.Mock
-// }
+// MockConfigLoader é uma interface mock para ConfigLoader
+type MockConfigLoader struct {
+	mock.Mock
+}
 
-// // GetObject mocks a chamada para GetObject do cliente S3
-// func (m *MockS3Client) GetObject(ctx context.Context, params *s3.GetObjectInput, optFns ...func(*s3.Options)) (*s3.GetObjectOutput, error) {
-// 	args := m.Called(ctx, params)
-// 	return args.Get(0).(*s3.GetObjectOutput), args.Error(1)
-// }
+func (m *MockConfigLoader) Load() (*config.LambdaConfig, error) {
+	args := m.Called()
+	if args.Get(0) == nil {
+		return nil, args.Error(1)
+	}
+	return args.Get(0).(*config.LambdaConfig), args.Error(1)
+}
 
-// // MockSSMClient é um mock para o cliente SSM
-// type MockSSMClient struct {
-// 	mock.Mock
-// }
+// MockConfigLoadDefaultConfig é uma função mock para config.LoadDefaultConfig
+var MockConfigLoadDefaultConfig = func(ctx context.Context, optFns ...func(*awsConfig.LoadOptions) error) (aws.Config, error) {
+	return aws.Config{}, nil
+}
 
-// // GetParameter mocks a chamada para GetParameter do cliente SSM
-// func (m *MockSSMClient) GetParameter(ctx context.Context, params *ssm.GetParameterInput, optFns ...func(*ssm.Options)) (*ssm.GetParameterOutput, error) {
-// 	args := m.Called(ctx, params)
-// 	return args.Get(0).(*ssm.GetParameterOutput), args.Error(1)
-// }
+func TestLambdaConfigNewLoader(t *testing.T) {
+	content, _ = os.ReadFile(fmt.Sprintf("%s/%s", local, filename))
+}
 
-// // MockConfigLoader é uma interface mock para ConfigLoader
-// type MockConfigLoader struct {
-// 	mock.Mock
-// }
+func TestLambdaConfigLoader(t *testing.T) {
+	t.Run("Deve carregar o conteúdo do arquivo de template local template.yaml", func(t *testing.T) {
+		loader := LocalLoader{
+			Path: fmt.Sprintf("%s/%s", local, filename),
+		}
+		data, err := loader.Load()
 
-// func (m *MockConfigLoader) Load() (*LambdaConfig, error) {
-// 	args := m.Called()
-// 	if args.Get(0) == nil {
-// 		return nil, args.Error(1)
-// 	}
-// 	return args.Get(0).(*LambdaConfig), args.Error(1)
-// }
-
-// // MockConfigLoadDefaultConfig é uma função mock para config.LoadDefaultConfig
-// var MockConfigLoadDefaultConfig = func(ctx context.Context, optFns ...func(*config.LoadOptions) error) (aws.Config, error) {
-// 	// Será substituída em cada teste conforme necessário
-// 	return aws.Config{}, nil
-// }
-
-// var content []byte
-
-// func TestLambdaConfigNewLoader(t *testing.T) {
-// 	content, _ = os.ReadFile(local)
-// }
-
-// func TestLambdaConfigLoader(t *testing.T) {
-// 	t.Run("Deve carregar o conteúdo do arquivo de template local template.yaml", func(t *testing.T) {
-// 		loader := LocalLoader{
-// 			Path: local,
-// 		}
-// 		data, err := loader.Load()
-
-// 		assert.NoError(t, err)
-// 		assert.NotNil(t, data)
-// 	})
-// }
-
-// func TestLambdaConfigS3Loader(t *testing.T) {
-// 	t.Run("Deve carregar o conteúdo do arquivo de template em s3://meu_bucket/template.yaml", func(t *testing.T) {
-// 		mockS3Client := new(MockS3Client)
-// 		mockS3Client.On("GetObject", mock.Anything, &s3.GetObjectInput{
-// 			Bucket: &bucket,
-// 			Key:    &local,
-// 		}).Return(&s3.GetObjectOutput{
-// 			Body: io.NopCloser(strings.NewReader(string(content))),
-// 		}, nil)
-
-// 		loader := &S3Loader{
-// 			Path:   fmt.Sprintf("s3://%s/%s", bucket, local),
-// 			Client: mockS3Client,
-// 		}
-// 		data, err := loader.Load()
-
-// 		assert.NoError(t, err)
-// 		assert.NotNil(t, data)
-// 	})
-// }
-
-// func TestLambdaSSMConfigLoader(t *testing.T) {
-// 	var (
-// 		withDecryption bool   = true
-// 		value          string = string(content)
-// 	)
-
-// 	t.Run("Deve carregar o conteúdo do arquivo de template em ssm://template_data", func(t *testing.T) {
-// 		mockSSMClient := new(MockSSMClient)
-// 		mockSSMClient.On("GetParameter", mock.Anything, &ssm.GetParameterInput{
-// 			Name:           &parameter,
-// 			WithDecryption: &withDecryption,
-// 		}).Return(&ssm.GetParameterOutput{
-// 			Parameter: &types.Parameter{
-// 				Value: &value,
-// 			},
-// 		}, nil)
-
-// 		loader := &SSMLoader{
-// 			Path:   parameter,
-// 			Client: mockSSMClient,
-// 		}
-// 		data, err := loader.Load()
-
-// 		assert.NoError(t, err)
-// 		assert.NotNil(t, data)
-// 	})
-// }
+		assert.NoError(t, err)
+		assert.NotNil(t, data)
+	})
+}
--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 set -e
 
 GOOS=linux GOARCH=amd64 go build -o bin/bootstrap cmd/lambda/main.go
-sam local start-api --debug --skip-pull-image --warm-containers LAZY
+sam local start-api --skip-pull-image --warm-containers LAZY
 # curl -X POST http://127.0.0.1:3000/ -d '{"teste":123}'
